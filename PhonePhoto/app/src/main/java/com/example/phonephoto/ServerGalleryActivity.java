0d3f13d900448c807f457982163ecc0a1167e698
zzompang2 <58748144+zzompang2@users.noreply.github.com>
2020-07-19 08:01:06 +0900

    서버 전체 사진 비트맵으로 가져옴

--- PhonePhoto/app/src/main/java/com/example/phonephoto/ServerGalleryActivity.java
+++ PhonePhoto/app/src/main/java/com/example/phonephoto/ServerGalleryActivity.java
@@ -13,15 +13,8 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.phonephoto.data.DownloadResponse;
-import com.example.phonephoto.data.UploadResponse;
 import com.example.phonephoto.network.RetrofitClient;
 import com.example.phonephoto.network.ServiceApi;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -40,36 +33,11 @@ public class ServerGalleryActivity extends AppCompatActivity {
         setContentView(R.layout.activity_server_gallery);
 
         imageView = findViewById(R.id.imageView);
-
-        /*
-        // 파일 받기: Thread 사용
-        Thread thread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try{
-                    URL url = new URL("http://192.249.19.242:7080/users/1594779081800.jpg");
-                    InputStream inputStream = url.openStream();
-                    final Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
-                    handler.post(new Runnable() {
-                        @Override
-                        public void run() {  // 화면에 그려줄 작업
-                            imageView.setImageBitmap(bitmap);
-                        }
-                    });
-                    imageView.setImageBitmap(bitmap); //비트맵 객체로 보여주기
-                } catch (Exception e) {
-
-                }
-            }
-        });
-        thread.start();
-         */
-
-        downloadFile();
+        showFile();
     }
 
-    private void downloadFile() {
-        Log.d(TAG, "downloadFile");
+    private void showFile() {
+        Log.d(TAG, "showFile");
         ServiceApi getResponse = RetrofitClient.getRetrofit().create(ServiceApi.class);
         Call<DownloadResponse> call = getResponse.downloadFile();
 
@@ -80,30 +48,22 @@ public class ServerGalleryActivity extends AppCompatActivity {
             public void onResponse(Call<DownloadResponse> call, Response<DownloadResponse> response) {
                 Log.d(TAG, "onResponse");
 
-                //boolean writtenToDisk = writeResponseBodyToDisk(response.body());
-
-//                DownloadResponse downloadResponse = response.body();
-//
-//                Log.d(TAG, String.valueOf(downloadResponse));
-//
-//                Bitmap bitmap = downloadResponse.getBitmap();
-//                imageView.setImageBitmap(bitmap);
-
                 DownloadResponse downloadResponse = response.body();
                 Log.d(TAG, String.valueOf(downloadResponse));
+
                 if (downloadResponse != null) {
                     Log.d(TAG, "downloadResponse 받았다!");
-                    if (response.isSuccessful()) {
-                       // Toast.makeText(getApplicationContext(), downloadResponse.getName(), Toast.LENGTH_SHORT).show();
-                        //Log.d(TAG, "img name: " + downloadResponse.getName());
-                        byte[] bytes = Base64.decode(response.body().getImg(), Base64.DEFAULT);
+                    String[] imgArray = downloadResponse.getImg();
+                    int num = downloadResponse.getName().length;
+                    Log.d(TAG, String.valueOf(num));
+
+                    Log.d(TAG, String.valueOf(imgArray));
+
+                    for(int i=0; i<num; i++) {
+                        byte[] bytes = Base64.decode(imgArray[i], Base64.DEFAULT);
                         Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
                         imageView.setImageBitmap(bitmap);
-
-                    } else {
-                        //Toast.makeText(getApplicationContext(), downloadResponse.getName(), Toast.LENGTH_SHORT).show();
                     }
-                    //Log.d(TAG, downloadResponse.getImg());
 
                 } else {
                     // Call은 제대로 보냈으나 서버에서 이거뭐냐? 하고 reponse를 보낸 경우 (????)
